ebaee8a010a93a602422031d2b04fb4ecaf21d8f
ArpitLokwani <arpit lokwani>
2020-02-26 09:23:45 +0530

    Initial Commit

---
+++ GenericsInSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericsInSwift
+//
+//  Created by Arpit Lokwani on 26/02/20.
+//  Copyright © 2020 Arpit Lokwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
